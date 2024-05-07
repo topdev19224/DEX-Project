d1503d340f705ddeb7cd8c494d7ec95a26e1b506
SoftwareVeteran <38038268+SoftwareVeteran@users.noreply.github.com>
2024-05-07 04:03:45 -0700

    updated header file

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Avatar, Popover, Row, Switch } from 'antd';
-import defaultAvatar from '../../assets/images/avatar0.jpg';
+import React from 'react'
+import { Avatar, Popover, Row, Switch } from 'antd'
+import defaultAvatar from '../../assets/images/avatar0.jpg'
 import playBtn from '../../assets/images/play.png';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
